50fe17a8899669774d0046e1750ca96d173b9a9a
AmanuelAy <amanuelayalew34@gmail.com>
2022-09-21 06:07:05 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -21,20 +21,28 @@ int main(void)
 		sum += password[index++];
 	}
 
-	password[index++] = '\0';
+	password[index] = '\0';
 
 	if (sum != 2772)
 	{
-		diff_half1 = (sum -2772) / 2;
+		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 
 		for (index = 0; passwords[index]; index++)
 		{
-			if (password[index++] >= (33 + diff_half1))
+			if (password[index] >= (33 + diff_half1))
 			{
-				(password[index] -= diff_half2;
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; passwords[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
 				break;
 			}
 		}
